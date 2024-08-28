f928ccfd282af89eeb5d3c4dc6e6e4e734a11f23
Prabhav pandey <62758839+prabhavpandey19@users.noreply.github.com>
2024-08-29 00:07:54 +0530

    Runtime: 13 ms (faster than 5.21%), Memory: 42.9 MB (less than 100.0%) - GitSync

---
+++ 508-most-frequent-subtree-sum/508. Most Frequent Subtree Sum.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+   Map<Integer,Integer> map = new HashMap();
+	 int m=0;
+	 public int[] findFrequentTreeSum(TreeNode root) {
+	        postorder(root);
+	        ArrayList<Integer> temp = new ArrayList();
+	        for(int i : map.keySet()){
+	            if(map.get(i) == m)temp.add(i); 
+	        }
+	        
+	        int []res = new int[temp.size()];
+	        for(int i = 0;i< temp.size();i++){
+	            res[i] = temp.get(i);
+	        }
+	        return res;
+	    }
+	private int postorder(TreeNode root) {
+		if(root==null)return 0;
+		int left=postorder(root.left);
+		int right=postorder(root.right);
+		int sum=left+right+root.val;
+		map.put(sum, map.getOrDefault(sum, 0)+1);
+		m = Math.max(m,map.get(sum));
+		return sum;
+		
+	}
+}
